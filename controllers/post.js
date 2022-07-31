d58e9f147d39af7da52bdd5e0e99323ea66c0a3d
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-07-31 11:12:09 +0100

    date

--- controllers/post.js
+++ controllers/post.js
@@ -8,7 +8,7 @@ const JWT_SECRET = 'jdfuqgwefouh@#$%jknskdjhu%$^jasbdjqd376@!%sdlfj';
 //CREATING A NEW POST
 const createNewPost = async (req, res) => {
     const date = new Date()
-    date.toDateString()
+    
 
     // cloudinary.config({
     //     cloud_name: "wilsonchinedu",
@@ -25,7 +25,7 @@ const createNewPost = async (req, res) => {
                 category: req.body.category,
                 title: req.body.title,
                 author: req.body.author,
-                date: date,
+                date: date.toDateString(),
                 thumbnail: req.body.thumbnail,
                 thumbnail2: req.body.thumbnail2,
                 news: req.body.news,

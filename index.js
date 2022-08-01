ab565151f324f49f1e5ee12226d956b6814fd423
best-developer-in-africa <wilsonajikwu12@gmail.com>
2022-08-01 09:50:04 +0100

    Link

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ let port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+app.use(require('prerender-node').set('prerenderToken', 'f57zFS6y0ReqJqfsF6is'));
+
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
